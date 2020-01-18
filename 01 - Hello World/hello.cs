0d5fb7420af6e2c520c3224f723c150bba880ae7
Dogan Yigit Yenigun <yigit_yenigun@hotmail.com>
2020-01-18 20:44:19 +0300

    Hello World in C#

---
+++ 01 - Hello World/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is Github!");
+        }
+    }
+}
